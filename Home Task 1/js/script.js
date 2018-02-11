b6f25f3701c2a6fecd869de062c5367cee1277d3
Pugach2017 <santangels2@gmail.com>
2018-02-11 16:29:23 +0200

    Index

---
+++ Home Task 1/js/script.js
@@ -0,0 +1,24 @@
+const lineUp = 'qwertyuiop[]';
+const lineMiddle = 'asdfghjkl;';
+const lineBottom = 'zxcvbnm,./';
+
+/*console.log(lineUp.length);
+console.log(lineMiddle.length);
+console.log(lineBottom.length);*/
+
+const up = '12';
+const middle ='10';
+const bottom = '10';
+
+console.log(lineUp.charAt (0));
+console.log(lineUp.charAt (11));
+
+console.log(lineMiddle.charAt (0));
+console.log(lineMiddle.charAt (9));
+
+console.log(lineBottom.charAt (0));
+console.log(lineBottom.charAt (9));
+
+console.log(lineUp.indexOf('[]'));
+
+console.log(lineUp, lineMiddle, lineBottom, up, middle, bottom);
